285d517fb03391f2c23fb8ef57cf84346f3f1ca8
harshurampur <harshu.rampur@ga.gov.au>
2019-01-18 15:40:45 +1100

    add more tests

--- build.sh
+++ build.sh
@@ -5,20 +5,21 @@ cd docker
 
 # get the latest config
 pushd config
-#wget https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/dev/services/wms/ows/wms_cfg.py -O wms_cfg.py
-wget https://gist.githubusercontent.com/harshurampur/bebb0e718d8090fea0247f37909bd2eb/raw/e183955c8ab7b5a6bcdbc4f8e085928983062499/wms_cfg.py -O wms_cfg.py
+wget https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/dev/services/wms/ows/wms_cfg.py -O wms_cfg.py
+#wget https://gist.githubusercontent.com/harshurampur/bebb0e718d8090fea0247f37909bd2eb/raw/e183955c8ab7b5a6bcdbc4f8e085928983062499/wms_cfg.py -O wms_cfg.py
 popd
+
 
 # Run the index job
 docker-compose up -d
 
 # Wait until the database is loaded
 
-until docker logs docker_index_1 | grep -m 1 "Done"; do sleep 10; done
+until docker logs docker_index_1 | grep -m 1 "Updating range for:  low_tide_comp_20p"; do sleep 10; done
 
 # Save the image with built database
-docker stop docker_index_1
-docker commit postgres opendatacube/dea-index:$(date +%Y-%m-%d)
+#docker stop docker_postgres_1
+docker commit docker_index_1 opendatacube/dea-index:$(date +%Y-%m-%d)
 docker tag opendatacube/dea-index:$(date +%Y-%m-%d) opendatacube/dea-index:latest
 
 # test it out 
